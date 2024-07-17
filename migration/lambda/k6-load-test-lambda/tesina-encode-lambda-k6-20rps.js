a99f5bf7f2de0e24d3b8d4bc3071faec0a881a6e
Julian Casaburi <juliancasaburi@gmail.com>
2024-07-17 19:36:54 -0300

    Add migration K6 benchmark configs

---
+++ migration/lambda/k6-load-test-lambda/tesina-encode-lambda-k6-20rps.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+
+export let options = {
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 20, // RPS
+      timeUnit: '1s',
+      duration: '1m',
+      preAllocatedVUs: 20,
+      maxVUs: 100,
+    },
+  },
+};
+
+export default function () {
+  const url = 'https://bvazo3cbx9.execute-api.us-east-1.amazonaws.com/Prod/tesina-encode-lambda';
+  const payload = JSON.stringify({ text: 'Hello, World!' });
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  http.post(url, payload, params);
+}
