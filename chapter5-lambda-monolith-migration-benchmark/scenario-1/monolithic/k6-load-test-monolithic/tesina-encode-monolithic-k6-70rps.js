1e225a8c0c350c5fdb396ca65f6b02e96e29bca2
Julian Casaburi <juliancasaburi@gmail.com>
2024-09-19 05:50:44 -0300

    Add i/o bound migration files
    Signed-off-by: Julian Casaburi <juliancasaburi@gmail.com>

--- chapter5-lambda-monolith-migration-benchmark/scenario-1/monolithic/k6-load-test-monolithic/tesina-encode-monolithic-k6-70rps.js
+++ chapter5-lambda-monolith-migration-benchmark/scenario-1/monolithic/k6-load-test-monolithic/tesina-encode-monolithic-k6-70rps.js
@@ -4,7 +4,7 @@ export let options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 50, // RPS
+      rate: 70, // RPS
       timeUnit: '1s',
       duration: '1m',
       preAllocatedVUs: 20,
@@ -14,7 +14,7 @@ export let options = {
 };
 
 export default function () {
-  const url = 'https://c7itsddefi.execute-api.us-east-1.amazonaws.com/prod/encode';
+  const url = 'https://ryz6r0xfda.execute-api.us-east-1.amazonaws.com/prod/encode';
   const payload = JSON.stringify({ text: 'Hello, World!' });
   const params = {
     headers: {
