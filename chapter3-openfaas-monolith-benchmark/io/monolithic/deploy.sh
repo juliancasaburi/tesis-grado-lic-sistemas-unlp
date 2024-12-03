b48dc682a57981f008dc0f8b035ddb9a9547e543
Julian Casaburi <juliancasaburi@gmail.com>
2024-12-03 02:13:03 -0300

    Include secrets creation for chapter3-openfaas-monolith-benchmark/io/monolithic
    Signed-off-by: Julian Casaburi <juliancasaburi@gmail.com>

--- chapter3-openfaas-monolith-benchmark/io/monolithic/deploy.sh
+++ chapter3-openfaas-monolith-benchmark/io/monolithic/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kubectl apply -f dynamodb-secrets.yaml -n monolithic
+
 kubectl apply -f service.yml -f deployment.yml -n monolithic
 
 kubectl -n monolithic patch svc tesina-scenario-network-monolithic-service -p '{"spec": {"type": "LoadBalancer"}}'
